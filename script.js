aa463be1b2a5b4aeeda1216a0bf7755b35c69e84
Lorenzo C <wolf94-@libero.it>
2022-10-24 15:52:30 +0200

    prova di timing function

---
+++ script.js
@@ -0,0 +1,74 @@
+const slider = document.querySelector('.slider');
+const thumbs = document.querySelector('.thumbs');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+const numImages = 5;
+let counterImages = 0;
+let sliderHtml = '';
+let thumbsHtml = '';
+
+for(let i = 1; i <= numImages; i++){
+  sliderHtml += `
+      <img  class="item" src="img/0${i}.jpg" alt="">
+  `;
+  thumbsHtml += `
+      <img  class="item-thumb" src="img/0${i}.jpg" alt="">
+  `;
+}
+
+slider.innerHTML = sliderHtml;
+thumbs.innerHTML = thumbsHtml;
+
+const listImages = document.getElementsByClassName('item');
+const listthumbs = document.getElementsByClassName('item-thumb');
+
+listImages[counterImages].classList.add('active');
+listthumbs[counterImages].classList.add('active');
+
+
+/*const output = document.querySelector('.output');
+
+let countDown = 5;
+
+setTimeout(function() {
+  clearInterval(clock);
+}, (countDown + 1) * 1000);
+
+
+const clock = setInterval(function() {
+  countDown--;
+  // if (countDown < 0) countDown = 0;
+  printOutput(countDown);
+  // if(countDown === 0) {
+  //   clearInterval(clock);
+  // }
+}, 1000);
+
+
+
+
+
+
+printOutput(countDown);*/
+
+//problemi nel log
+let countDown = setInterval(next, 5000);
+
+
+prev.addEventListener('click',function(){
+  listImages[counterImages].classList.remove('active');
+  listthumbs[counterImages].classList.remove('active');
+  counterImages++;
+  if(counterImages === numImages) counterImages = 0;
+  listImages[counterImages].classList.add('active');
+  listthumbs[counterImages].classList.add('active');
+})
+next.addEventListener('click',function(){
+  listImages[counterImages].classList.remove('active');
+  listthumbs[counterImages].classList.remove('active');
+  counterImages--;
+  if(counterImages < 0) counterImages = numImages - 1;
+  listImages[counterImages].classList.add('active');
+  listthumbs[counterImages].classList.add('active');
+})
